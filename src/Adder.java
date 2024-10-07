7120cad0480222730e9c3bec3989ad98a9f1a047
s21806 <s21806@pjwstk.edu.pl>
2024-10-07 18:54:35 +0200

    Populated Adder Class

--- src/Adder.java
+++ src/Adder.java
@@ -3,16 +3,4 @@ public class Adder {
         {
             return (a+b);
         }
-        int add(int a,int b,int c)
-        {
-            return (a+b+c);
-        }
-        public static void main(String args[ ])
-        {
-            Adder ad=new Adder( );
-            ad.add(40,60);
-            System.out.println("Sum="+ad.add());
-            ad.add(40,60,20);
-            System.out.println("Sum= "+ad.add());
-        }
     }
