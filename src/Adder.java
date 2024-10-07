cfcfeea4f53395f689036588a70b69218d9183da
s21806 <s21806@pjwstk.edu.pl>
2024-10-07 18:53:54 +0200

    Specific who adds 'Adder' and who adds 'Subtractor' classes

--- src/Adder.java
+++ src/Adder.java
@@ -1,2 +1,18 @@
 public class Adder {
-}
+        int add(int a,int b)
+        {
+            return (a+b);
+        }
+        int add(int a,int b,int c)
+        {
+            return (a+b+c);
+        }
+        public static void main(String args[ ])
+        {
+            Adder ad=new Adder( );
+            ad.add(40,60);
+            System.out.println("Sum="+ad.add());
+            ad.add(40,60,20);
+            System.out.println("Sum= "+ad.add());
+        }
+    }
